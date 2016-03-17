d9353e31e600f6816ac48f43ddafd3d12aa2bffd
Noel Tock <hello@noeltock.com>
2016-03-17 09:56:22 +0700

    Initial - Underscores

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Fuerteventura_Dog_Rescue
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'fuerte' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'fuerte' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'fuerte' ), 'fuerte', '<a href="http://www.noeltock.com" rel="designer">Noel Tock</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
