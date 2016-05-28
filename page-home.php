b71869d3c6a64cb87e92cf3adb12d94b54737187
Noel Tock <hello@noeltock.com>
2016-05-28 14:02:20 +0200

    Update slugs

--- page-home.php
+++ page-home.php
@@ -15,9 +15,9 @@ get_header(); ?>
 					</div>
 					<p>If you live on the island or abroad, we have plenty of dogs that need new homes.</p>
 					<ul>
-						<li><a href="<?php site_url(); ?>">View adoptions</a></li>
-						<li><a href="<?php site_url(); ?>">Before you adopt</a></li>
-						<li><a href="<?php site_url(); ?>">Adopting from outside of Spain</a></li>
+						<li><a href="<?php site_url(); ?>/adoptions/">View adoptions</a></li>
+						<li><a href="<?php site_url(); ?>/adoptions/">Before you adopt</a></li>
+						<li><a href="<?php site_url(); ?>/adoptions/">Adopting from outside of Spain</a></li>
 					</ul>
 				</div>
 				<div class="home-toc-item">
@@ -26,9 +26,9 @@ get_header(); ?>
 					</div>
 					<p>We are completely volunteer-powered and all donations go to the benefit of the welfare of the dogs.</p>
 					<ul>
-						<li><a href="<?php site_url(); ?>">Donate today</a></li>
-						<li><a href="<?php site_url(); ?>">Become a member</a></li>
-						<li><a href="<?php site_url(); ?>">Sponsor a dog</a></li>
+						<li><a href="<?php site_url(); ?>/donate/">Donate today</a></li>
+						<li><a href="<?php site_url(); ?>/donate/">Become a member</a></li>
+						<li><a href="<?php site_url(); ?>/donate/">Sponsor a dog</a></li>
 				</div>
 				<div class="home-toc-item">
 					<div class="home-toc-icon">
@@ -36,10 +36,10 @@ get_header(); ?>
 					</div>
 					<p>We're in need of volunteers for a number of tasks, see how you can help below:</p>
 					<ul>
-						<li><a href="<?php site_url(); ?>">Volunteer at the shelter</a></li>
-						<li><a href="<?php site_url(); ?>">Foster a dog at your home</a></li>
-						<li><a href="<?php site_url(); ?>">Run a local business?</a></li>
-						<li><a href="<?php site_url(); ?>">Flying out of Fuerteventura?</a></li>
+						<li><a href="<?php site_url(); ?>/volunteer/">Volunteer at the shelter</a></li>
+						<li><a href="<?php site_url(); ?>/adoptions/">Foster a dog at your home</a></li>
+						<li><a href="<?php site_url(); ?>/about-us/">Run a local business?</a></li>
+						<li><a href="<?php site_url(); ?>/volunteer/">Flying out of Fuerteventura?</a></li>
 				</div>
 			</div>
 
